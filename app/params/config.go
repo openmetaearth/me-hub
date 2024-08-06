f64a4b80a5c466ac2332c559c8da2da0237583a7
ningxin <ningxin1718@gmail.com>
2024-08-06 19:47:13 +0800

    fix: setup sh (#3)

--- app/params/config.go
+++ app/params/config.go
@@ -9,16 +9,16 @@ import (
 
 const (
 	// DisplayDenom is the denomination used to display the amount of tokens held
-	DisplayDenom = "dym"
+	DisplayDenom = "mec"
 	// BaseDenomUnit is the base denom unit for the Hub
-	BaseDenom = "adym"
+	BaseDenom = "umec"
 
 	// BaseDenomUnit defines the base denomination unit for Photons.
-	// 1 DYM = 1x10^{BaseDenomUnit} adym
-	BaseDenomUnit = 18
+	// 1 MEC = 1x10^{BaseDenomUnit} umec
+	BaseDenomUnit = 8
 
-	AccountAddressPrefix = "dym"
-	Name                 = "dymension"
+	AccountAddressPrefix = "mec"
+	Name                 = "mechain"
 )
 
 func init() {
