3f74cfd42b65d47a05b9178a69649dab84c903da
james <james.zhang.gb@gmail.com>
2024-08-29 11:45:29 +0800

    remove unused code

--- x/wdistri/keeper/params.go
+++ x/wdistri/keeper/params.go
@@ -3,7 +3,7 @@ package keeper
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/st-chain/me-hub/x/wdistri/types"
-	wmintTypes "github.com/st-chain/me-hub/x/wmint/types"
+
 )
 
 // GetParams get all parameters as types.Params
@@ -16,8 +16,3 @@ func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 	k.paramstore.SetParamSet(ctx, &params)
 }
 
-const oneDayTotalBlocks = wmintTypes.OneDayTotalBlocks
-
-const oneYearTotalBlocks = wmintTypes.OneYearTotalBlocks
-
-const initOneYearMintAmount = wmintTypes.InitOneYearMintAmount
