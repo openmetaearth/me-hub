70ad6e8bdc7c17e92bbcfeb66f62b95f98877658
Shing <shinnng@outlook.com>
2024-09-10 14:15:09 +0800

    test(kyc): add basic tests

---
+++ testutil/keeper/kyc.go
@@ -0,0 +1,42 @@
+package keeper
+
+import (
+	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
+	"github.com/cosmos/cosmos-sdk/testutil/testdata"
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/store"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/st-chain/me-hub/x/kyc/keeper"
+	"github.com/st-chain/me-hub/x/kyc/types"
+
+	cometbftdb "github.com/cometbft/cometbft-db"
+	"github.com/cometbft/cometbft/libs/log"
+	cometbftproto "github.com/cometbft/cometbft/proto/tendermint/types"
+	"github.com/stretchr/testify/require"
+)
+
+func KycKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
+	storeKey := sdk.NewKVStoreKey(types.StoreKey)
+	memStoreKey := storetypes.NewMemoryStoreKey(types.MemStoreKey)
+
+	db := cometbftdb.NewMemDB()
+	stateStore := store.NewCommitMultiStore(db)
+	stateStore.MountStoreWithDB(storeKey, storetypes.StoreTypeIAVL, db)
+	stateStore.MountStoreWithDB(memStoreKey, storetypes.StoreTypeMemory, nil)
+	require.NoError(t, stateStore.LoadLatestVersion())
+
+	registry := codectypes.NewInterfaceRegistry()
+	registry.RegisterInterface("/ethermint.crypto.v1.ethsecp256k1.PubKey",
+		(*cryptotypes.PubKey)(nil),
+		&testdata.Dog{})
+	cdc := codec.NewProtoCodec(registry)
+
+	k := keeper.NewKeeper(cdc, storeKey, nil, nil, nil)
+	ctx := sdk.NewContext(stateStore, cometbftproto.Header{}, false, log.NewNopLogger())
+
+	return k, ctx
+}
