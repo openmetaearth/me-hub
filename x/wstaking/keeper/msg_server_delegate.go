a6f91de73b83da62f14e994a691fe5ffffc27754
evanshapiro8 <evanshapiro8@outlook.com>
2024-09-14 14:31:00 +0800

    refactor delegation

--- x/wstaking/keeper/msg_server_delegate.go
+++ x/wstaking/keeper/msg_server_delegate.go
@@ -18,19 +18,14 @@ import (
 // Delegate defines a method for performing a delegation of coins from a delegator to a validator
 func (k MsgServer) Delegate(goCtx context.Context, msg *stakingtypes.MsgDelegate) (*stakingtypes.MsgDelegateResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-	did, ok := k.KycKeeper.GetDID(ctx, sdk.MustAccAddressFromBech32(msg.DelegatorAddress))
+	did, _ := k.KycKeeper.GetDID(ctx, sdk.MustAccAddressFromBech32(msg.DelegatorAddress))
+	kycData, ok := k.KycKeeper.GetKYC(ctx, did)
+	var regionId string
 	if !ok {
-		return k.UnMeidDelegate(goCtx, msg)
+		regionId = strings.ToLower(types.ExperienceRegionName)
 	} else {
-		return k.MeidDelegate(goCtx, msg, did)
+		regionId = string(kycData.Data)
 	}
-}
-
-// MeidDelegate defines a method for performing a delegation of coins from a KYC to a validator
-func (k MsgServer) MeidDelegate(goCtx context.Context, msg *stakingtypes.MsgDelegate, did string) (*stakingtypes.MsgDelegateResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
-	kycData, _ := k.KycKeeper.GetKYC(ctx, did)
-	regionId := string(kycData.Data)
 	region, isFound := k.GetRegion(ctx, regionId)
 	if !isFound {
 		return nil, types.ErrRegionNotExist
@@ -67,15 +62,10 @@ func (k MsgServer) MeidDelegate(goCtx context.Context, msg *stakingtypes.MsgDele
 		)
 	}
 
-	del := k.Delegation(ctx, delegatorAddress, sdk.ValAddress{})
+	delegation, isOK := k.GetDelegation(ctx, delegatorAddress, sdk.ValAddress{})
 	rewards := sdk.ZeroDec()
 	var regionTreasureAddr sdk.AccAddress
-	if del != nil {
-		delegation, isOK := del.(stakingtypes.Delegation)
-		if !isOK {
-			panic("withdrawDelegationRewards err:type Delegation assertion failed")
-			return nil, types.ErrAssertionFailed
-		}
+	if isOK {
 		rewards, err = k.CalculateInterest(ctx, delegation.Amount.Add(delegation.UnMeidAmount).Add(delegation.Unmovable), delegation.StartHeight)
 		if err != nil {
 			return nil, types.ErrCalculateInterest.Wrap(err.Error())
@@ -97,7 +87,7 @@ func (k MsgServer) MeidDelegate(goCtx context.Context, msg *stakingtypes.MsgDele
 	}
 
 	// NOTE: source funds are always UnBonded
-	newShares, err := k.Keeper.Delegate(ctx, delegatorAddress, msg.Amount.Amount, stakingtypes.Unbonded, validator, true)
+	newShares, err := k.Keeper.Delegate(ctx, delegatorAddress, msg.Amount.Amount, stakingtypes.Unbonded, validator, delegation)
 	if err != nil {
 		return nil, err
 	}
@@ -130,106 +120,5 @@ func (k MsgServer) MeidDelegate(goCtx context.Context, msg *stakingtypes.MsgDele
 		),
 	})
 
-	return &stakingtypes.MsgDelegateResponse{}, nil
-}
-
-func (k MsgServer) UnMeidDelegate(goCtx context.Context, msg *stakingtypes.MsgDelegate) (*stakingtypes.MsgDelegateResponse, error) {
-	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	region, isFound := k.GetRegion(ctx, strings.ToLower(types.ExperienceRegionName))
-	if !isFound {
-		return nil, types.ErrRegionNotExist
-	}
-	msg.ValidatorAddress = region.OperatorAddress
-
-	valAddr, valErr := sdk.ValAddressFromBech32(msg.ValidatorAddress)
-	if valErr != nil {
-		return nil, valErr
-	}
-	err := types.CheckMinDelegate(msg.Amount.Amount)
-	if err != nil {
-		return nil, err
-	}
-
-	validator, found := k.GetValidator(ctx, valAddr)
-	if !found {
-		return nil, stakingtypes.ErrNoValidatorFound
-	}
-
-	if validator.Tokens.LT(validator.DelegationAmount) {
-		return nil, types.ErrNodeLimitExceeded
-	}
-	delegatorAddress, err := sdk.AccAddressFromBech32(msg.DelegatorAddress)
-	if err != nil {
-		return nil, err
-	}
-
-	validator.DelegationAmount = validator.DelegationAmount.Add(msg.Amount.Amount)
-	region.DelegateAmount = region.DelegateAmount.Add(msg.Amount.Amount)
-
-	bondDenom := k.BondDenom(ctx)
-	if msg.Amount.Denom != bondDenom {
-		return nil, sdkerrors.Wrapf(
-			sdkerrors.ErrInvalidRequest, "invalid coin denomination: got %s, expected %s", msg.Amount.Denom, bondDenom,
-		)
-	}
-
-	del := k.Delegation(ctx, delegatorAddress, sdk.ValAddress{})
-	rewards := sdk.ZeroDec()
-	var regionTreasureAddr sdk.AccAddress
-	if del != nil {
-		delegation, isOK := del.(stakingtypes.Delegation)
-		if !isOK {
-			return nil, types.ErrAssertionFailed
-		}
-		rewards, err = k.CalculateInterest(ctx, delegation.Amount.Add(delegation.UnMeidAmount).Add(delegation.Unmovable), delegation.StartHeight)
-		if err != nil {
-			return nil, types.ErrCalculateInterest.Wrap(err.Error())
-		}
-		regionTreasureAddr, err = sdk.AccAddressFromBech32(region.RegionTreasureAddr)
-		if err != nil {
-			return nil, err
-		}
-		err = k.BankKeeper.SendCoins(ctx, regionTreasureAddr, delegatorAddress, sdk.NewCoins(sdk.NewCoin(params.BaseDenom, rewards.TruncateInt())))
-		if err != nil {
-			return nil, err
-		}
-		if region.DelegateInterest.GTE(rewards) {
-			region.DelegateInterest = region.DelegateInterest.Sub(rewards)
-		}
-	}
-
-	// NOTE: source funds are always unbonded
-	newShares, err := k.Keeper.UnMeidDelegate(ctx, delegatorAddress, msg.Amount.Amount, validator)
-	if err != nil {
-		return nil, err
-	}
-	k.SetRegion(ctx, region)
-	k.SetValidator(ctx, validator)
-
-	if msg.Amount.Amount.IsInt64() {
-		defer func() {
-			telemetry.IncrCounter(1, types.ModuleName, "delegate")
-			telemetry.SetGaugeWithLabels(
-				[]string{"tx", "msg", msg.Type()},
-				float32(msg.Amount.Amount.Int64()),
-				[]metrics.Label{telemetry.NewLabel("denom", msg.Amount.Denom)},
-			)
-		}()
-	}
-
-	ctx.EventManager().EmitEvents(sdk.Events{
-		sdk.NewEvent(
-			types.EventTypeUnMeidDelegate,
-			sdk.NewAttribute(types.AttributeKeyValidator, msg.ValidatorAddress),
-			sdk.NewAttribute(sdk.AttributeKeyAmount, msg.Amount.String()),
-			sdk.NewAttribute(types.AttributeKeyRewards, rewards.TruncateInt().String()+params.BaseDenom),
-			sdk.NewAttribute(types.AttributeKeyRegionTreasure, regionTreasureAddr.String()),
-			sdk.NewAttribute(types.AttributeKeyNewShares, newShares.String()),
-			sdk.NewAttribute(types.AttributeKeyTotalAmountDelegate, validator.DelegationAmount.String()+params.BaseDenom),
-			sdk.NewAttribute(sdk.AttributeKeySender, msg.DelegatorAddress),
-			sdk.NewAttribute(types.AttributeKeyRegionId, region.RegionId),
-		),
-	})
 	return &stakingtypes.MsgDelegateResponse{}, nil
 }
