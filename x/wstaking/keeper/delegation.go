06feb28ae126ba3f652fa067b156632b54cda67d
evanshapiro8 <evanshapiro8@outlook.com>
2024-11-11 11:32:17 +0800

    fix undelegation no seed rewards err

--- x/wstaking/keeper/delegation.go
+++ x/wstaking/keeper/delegation.go
@@ -11,36 +11,6 @@ import (
 	"time"
 )
 
-// GetAllUnMeidDelegationAmount
-func (k Keeper) GetAllUnMeidDelegationAmount(ctx sdk.Context) sdk.Coin {
-	store := ctx.KVStore(k.storeKey)
-	key := types.KeyPrefix(types.ExperienceRegionName + types.PrefixUnMeid)
-	var coin sdk.Coin
-	value := store.Get(key)
-	if value == nil {
-		ctx.Logger().Debug("get all UnMeidDelegationAmount err", "not found value by key", key)
-		return coin
-	}
-	err := coin.Unmarshal(value)
-	if err != nil {
-		ctx.Logger().Error("coin unmarshal err=", err.Error())
-	}
-	return coin
-}
-
-// SetAllUnMEIDDelegationAmount
-func (k Keeper) SetAllUnMEIDDelegationAmount(ctx sdk.Context, coin sdk.Coin) {
-	store := ctx.KVStore(k.storeKey)
-	b, err := coin.Marshal()
-	if err != nil {
-		ctx.Logger().Error("coin marshal err=", err.Error())
-		return
-	}
-
-	key := types.KeyPrefix(types.ExperienceRegionName + types.PrefixUnMeid)
-	store.Set(key, b)
-}
-
 // Undelegate unbonds an amount of delegator shares from a given validator. It
 // will verify that the unbonding entries between the delegator and validator
 // are not exceeded and unbond the staked tokens (based on shares) by creating
@@ -63,14 +33,6 @@ func (k Keeper) Undelegate(ctx sdk.Context, delAddr sdk.AccAddress, valAddr sdk.
 		completionTime = ctx.BlockHeader().Time.Add(k.UnbondingTime(ctx))
 		ubd := k.SetUnbondingDelegationEntry(ctx, delAddr, valAddr, ctx.BlockHeight(), completionTime, returnAmount)
 		k.InsertUBDQueue(ctx, ubd, completionTime)
-
-		allAmount := k.GetAllUnMeidDelegationAmount(ctx)
-		if allAmount.Amount.IsNil() {
-			allAmount.Amount = sdk.ZeroInt()
-		}
-		allAmount.Amount = allAmount.Amount.Sub(returnAmount)
-		allAmount.Denom = k.BondDenom(ctx)
-		k.SetAllUnMEIDDelegationAmount(ctx, allAmount)
 	} else {
 		amt := sdk.NewCoin(params.BaseDenom, returnAmount)
 		err = k.BankKeeper.UndelegateCoinsFromModuleToAccount(ctx, types.BondedPoolName, delAddr, sdk.NewCoins(amt))
