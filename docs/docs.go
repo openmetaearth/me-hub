0a91e7bb45a0e25c78813e037e2cc99a2dd45db1
Aris <ningxin1718@gmail.com>
2024-11-20 15:50:02 +0800

    fix: openapi (#110)
    * refactor cmd
    
    * fix: openapi

--- docs/docs.go
+++ docs/docs.go
@@ -1,6 +1,39 @@
 package docs
 
-import "embed"
+import (
+	"embed"
+	"github.com/gorilla/mux"
+	httptemplate "html/template"
+	"net/http"
+)
 
 //go:embed static
 var Docs embed.FS
+
+//go:embed template
+var template embed.FS
+
+const (
+	apiFile   = "/static/openapi.yml"
+	indexFile = "template/index.tpl"
+)
+
+func RegisterOpenAPIService(appName string, rtr *mux.Router) {
+	rtr.Handle(apiFile, http.FileServer(http.FS(Docs)))
+	rtr.HandleFunc("/", handler(appName))
+}
+
+// handler returns an http handler that servers OpenAPI console for an OpenAPI spec at specURL.
+func handler(title string) http.HandlerFunc {
+	t, _ := httptemplate.ParseFS(template, indexFile)
+
+	return func(w http.ResponseWriter, req *http.Request) {
+		t.Execute(w, struct {
+			Title string
+			URL   string
+		}{
+			title,
+			apiFile,
+		})
+	}
+}
