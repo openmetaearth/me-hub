9dd35b2701fd0142fb0f874f9a3872285641ad26
Aris <ningxin1718@gmail.com>
2024-12-20 11:07:30 +0800

    fix: remove bank withdraw treasury txs (#134)

--- x/did/types/tx.pb.go
+++ x/did/types/tx.pb.go
@@ -1295,7 +1295,6 @@ func _Msg_RemoveVC_Handler(srv interface{}, ctx context.Context, dec func(interf
 	return interceptor(ctx, in, info, handler)
 }
 
-var Msg_serviceDesc = _Msg_serviceDesc
 var _Msg_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "metaearth.did.Msg",
 	HandlerType: (*MsgServer)(nil),
