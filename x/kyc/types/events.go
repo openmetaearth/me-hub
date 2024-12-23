bce2cdf4421dace6842edb6288946b505325dd3b
loysare <loysare@gmail.com>
2024-12-23 13:52:03 +0800

    add key level type

--- x/kyc/types/events.go
+++ x/kyc/types/events.go
@@ -2,9 +2,9 @@ package types
 
 import (
 	"fmt"
-	"strconv"
 
 	sdkTypes "github.com/cosmos/cosmos-sdk/types"
+	didTypes "github.com/st-chain/me-hub/x/did/types"
 )
 
 const (
@@ -16,12 +16,12 @@ const (
 	EventTypeDeleteSBT = "delete_sbt"
 )
 
-func NewKycEvent(address string, did string, level int, action string, seq uint64) sdkTypes.Event {
+func NewKycEvent(address string, did string, level didTypes.KycLevel, action string, seq uint64) sdkTypes.Event {
 	attributes := []sdkTypes.Attribute{
 		{Key: "sequence", Value: fmt.Sprintf("%d", seq)},
 		{Key: "address", Value: address},
 		{Key: "did", Value: did},
-		{Key: "level", Value: strconv.Itoa(level)},
+		{Key: "level", Value: level.String()},
 		{Key: "action", Value: action},
 	}
 	return sdkTypes.NewEvent("kyc_event", attributes...)
