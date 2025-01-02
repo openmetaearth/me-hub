29ef0c4809a181ccc59179c826a335dd072ec96b
lsq984 <lsq984@163.com>
2025-01-02 10:24:58 +0800

    fix::remove tx_group's debug print info

--- x/megroup/client/cli/tx_group.go
+++ x/megroup/client/cli/tx_group.go
@@ -38,7 +38,7 @@ func CmdCreateGroup() *cobra.Command {
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}
-			clientCtx.PrintProto(argGroupInfo)
+			//clientCtx.PrintProto(argGroupInfo)
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
 		},
 	}
