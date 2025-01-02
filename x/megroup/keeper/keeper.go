2bf832913cb681f2bab247cb440e93832a88eb68
lsq984 <lsq984@163.com>
2025-01-02 14:25:53 +0800

    repeate commit

--- x/megroup/keeper/keeper.go
+++ x/megroup/keeper/keeper.go
@@ -136,7 +136,7 @@ func (k Keeper) procKycRegionChange(sdkCtx sdk.Context, address, preRegionID, no
 		if !found {
 			return errors.Wrapf(types.ErrGroupNotExist, fmt.Sprintf("can not found joined previous gourp.groupID = %d", joined.GroupId))
 		}
-
+        //admin can not migrate
 		if address == preGroupInfo.Admin { //admin can not leave group
 			return errors.Wrapf(types.ErrExcute, "admin of group can not leave")
 		}
