bb253131dd35e507b7308bfa7391d2bf0bd8e594
evanshapiro8 <130117294+evanshapiro8@users.noreply.github.com>
2025-01-17 16:51:19 +0800

    Feat/record (#187)
    * add new new-record and review-recode
    
    * Modification of clock in transactions and review of open transactions
    
    * new Add transfer transaction
    
    * new add wstakinghooks Call during KYC update operation
    
    * Added a validator for the hooks function after transferring transactions DelegationAmount. Sub (delegation. Mount) operation
    
    * Add processing for transfer transactions and remove staking hooks in KYC update kyc_reward new add transferDeposit
    
    * Code organization to remove redundant code
    
    * refactoring transfer region logic
    
    ---------
    
    Co-authored-by: root <root@localhost.localdomain>
    Co-authored-by: maoaixiao1314 <m18634441821@163.com>

---
+++ x/wstaking/keeper/msg_server_record.go
@@ -0,0 +1,70 @@
+package keeper
+
+import (
+	"fmt"
+	"unicode"
+
+	sdkerrors "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/st-chain/me-hub/x/wstaking/types"
+	"golang.org/x/net/context"
+)
+
+func (k MsgServer) NewRecord(goCtx context.Context, msg *types.MsgNewRecord) (*types.MsgNewRecordResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	from, err := sdk.AccAddressFromBech32(msg.From)
+	if err != nil {
+		return nil, err
+	}
+	if msg.ActionNumber == "" {
+		return nil, types.ErrInvalidRecordParams.Wrap("invalid record number,is empty")
+	} else {
+		for _, r := range msg.ActionNumber {
+			// unicode.IsLetter check if it is a letter
+			// unicode.IsDigit check if it is a number
+			if !unicode.IsLetter(r) && !unicode.IsDigit(r) {
+				return nil, types.ErrInvalidRecordParams.Wrap("invalid record number,only letters and numbers are allowed")
+			}
+		}
+	}
+	if msg.ActionUrl == "" {
+		return nil, types.ErrInvalidRecordParams.Wrap("url is empty")
+	}
+	r := types.Record{
+		RecordNumber: msg.ActionNumber,
+		Url:          msg.ActionUrl,
+		From:         msg.From,
+	}
+	k.SetRecord(ctx, r, from)
+	return &types.MsgNewRecordResponse{}, nil
+}
+func (k MsgServer) ReviewRecord(goCtx context.Context, msg *types.MsgReviewRecord) (*types.MsgReviewRecordResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	globalAdmin := k.daoKeeper.GetGlobalDao(ctx)
+	meidAdmin := k.daoKeeper.GetMeidDao(ctx)
+	if globalAdmin != msg.From && meidAdmin != msg.From {
+		errLogBytes := fmt.Sprintf("review record account (%s) should  be global admin", msg.From)
+		return nil, sdkerrors.Wrapf(types.ErrParameter, errLogBytes)
+	}
+	_, err := sdk.AccAddressFromBech32(msg.From)
+	if err != nil {
+		return nil, err
+	}
+	if msg.ReviewResult == "" {
+		return nil, types.ErrInvalidRecordParams.Wrap("review result is empty")
+	}
+	if msg.RecordHash == "" {
+		return nil, types.ErrInvalidRecordParams.Wrap("invalid record hash,is empty")
+	}
+	if msg.ActionNumber == "" {
+		return nil, types.ErrInvalidRecordParams.Wrap("invalid record number,is empty")
+	}
+	rr := types.ReviewRecord{
+		RecordHash:      msg.RecordHash,
+		ActionNumber:    msg.ActionNumber,
+		RecordResult:    msg.ReviewResult,
+		ReviewedAddress: msg.ReviewedAddress,
+	}
+	k.SetReviewRecord(ctx, rr)
+	return &types.MsgReviewRecordResponse{}, nil
+}
--- x/wstaking/types/msgs.go
+++ x/wstaking/types/msgs.go
@@ -1,13 +1,14 @@
 package types
 
 import (
-	"cosmossdk.io/math"
 	"fmt"
+	gomath "math"
+
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/st-chain/me-hub/app/params"
-	gomath "math"
 )
 
 const (
@@ -16,11 +17,15 @@ const (
 	TypeMsgWithdrawDelegatorReward           = "withdraw_delegator_reward"
 	TypeMsgRemoveRegion                      = "remove-region"
 	TypeMsgRetrieveFeeFromGlobalAdminFeePool = "retrieve-fee-from-global-admin-fee-pool"
+	TypeMsgRecord                            = "new_record"
+	TypeReviewRecord                         = "review_record"
 	TypeMsgStake                             = "stake"
 	TypeMsgUnstake                           = "unstake"
 	TypeMsgWithdrawFromRegion                = "withdraw_from_region"
 	TypeMsgWithdrawFromGlobalDaoFeePool      = "withdraw_from_global_dao_fee_pool"
 	TypeMsgResetValidator                    = "create_validator"
+	TypeMsgNewMeid                           = "new_meid"
+	TypeMsgRemoveMeid                        = "remove_meid"
 )
 
 var (
@@ -274,6 +279,87 @@ func (msg *MsgWithdrawFromRegion) ValidateBasic() error {
 	return nil
 }
 
+// NewMsgRecord creates a new MsgNewRecord instance.
+func NewMsgRecord(actionNum, url, addr string) *MsgNewRecord {
+	return &MsgNewRecord{
+		ActionNumber: actionNum,
+		ActionUrl:    url,
+		From:         addr,
+	}
+}
+
+// Route implements the sdk.Msg interface.
+func (msg MsgNewRecord) Route() string { return RouterKey }
+
+// Type implements the sdk.Msg interface.
+func (msg MsgNewRecord) Type() string { return TypeMsgRecord }
+
+// GetSigners implements the sdk.Msg interface.
+func (msg MsgNewRecord) GetSigners() []sdk.AccAddress {
+	staker, _ := sdk.AccAddressFromBech32(msg.From)
+	return []sdk.AccAddress{staker}
+}
+
+// GetSignBytes implements the sdk.Msg interface.
+func (msg MsgNewRecord) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(&msg)
+	return sdk.MustSortJSON(bz)
+}
+
+// ValidateBasic implements the sdk.Msg interface.
+func (msg MsgNewRecord) ValidateBasic() error {
+	if _, err := sdk.AccAddressFromBech32(msg.From); err != nil {
+		return sdkerrors.ErrInvalidAddress.Wrapf("invalid staker address: %s", err)
+	}
+	return nil
+}
+
+// NewMsgReviewRecord creates a new MsgReviewRecord instance.
+func NewMsgReviewRecord(hash, result, address, id, reviewedAddress string) *MsgReviewRecord {
+	return &MsgReviewRecord{
+		RecordHash:      hash,
+		ReviewResult:    result,
+		From:            address,
+		ActionNumber:    id,
+		ReviewedAddress: reviewedAddress,
+	}
+}
+
+// Route implements the sdk.Msg interface.
+func (msg MsgReviewRecord) Route() string { return RouterKey }
+
+// Type implements the sdk.Msg interface.
+func (msg MsgReviewRecord) Type() string { return TypeReviewRecord }
+
+// GetSigners implements the sdk.Msg interface.
+func (msg MsgReviewRecord) GetSigners() []sdk.AccAddress {
+	staker, _ := sdk.AccAddressFromBech32(msg.From)
+	return []sdk.AccAddress{staker}
+}
+
+// GetSignBytes implements the sdk.Msg interface.
+func (msg MsgReviewRecord) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(&msg)
+	return sdk.MustSortJSON(bz)
+}
+
+// ValidateBasic implements the sdk.Msg interface.
+func (msg MsgReviewRecord) ValidateBasic() error {
+	if _, err := sdk.AccAddressFromBech32(msg.From); err != nil {
+		return sdkerrors.ErrInvalidAddress.Wrapf("invalid staker address: %s", err)
+	}
+	if msg.ReviewResult == "" {
+		return sdkerrors.ErrInvalidRequest.Wrap("invalid review result")
+	}
+	if msg.RecordHash == "" {
+		return sdkerrors.ErrInvalidRequest.Wrap("invalid record hash")
+	}
+	if msg.ActionNumber == "" {
+		return sdkerrors.ErrInvalidRequest.Wrap("invalid action number")
+	}
+	return nil
+}
+
 func NewMsgWithdrawFromGlobalDaoFeePool(withdrawer string, amount sdk.Coins) *MsgWithdrawFromGlobalDaoFeePool {
 	return &MsgWithdrawFromGlobalDaoFeePool{
 		Withdrawer: withdrawer,
@@ -373,5 +459,37 @@ func (msg MsgWithdrawDelegatorReward) ValidateBasic() error {
 		}
 	}
 
+	return nil
+}
+
+func NewMsgTransferRegion(from, to, creatorAddr string, address []string) *MsgTransferRegion {
+	return &MsgTransferRegion{FromRegion: from, ToRegion: to, Address: address, Creator: creatorAddr}
+}
+func (msg *MsgTransferRegion) Route() string {
+	return RouterKey
+}
+
+func (msg *MsgTransferRegion) Type() string {
+	return "msg_transfer_meid"
+}
+
+func (msg *MsgTransferRegion) GetSigners() []sdk.AccAddress {
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{creator}
+}
+
+func (msg *MsgTransferRegion) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+func (msg *MsgTransferRegion) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+	}
 	return nil
 }
