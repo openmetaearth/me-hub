ee1ab009ff1de856ae4fb8a6add26eb07e4eda70
aris <ningxin1718@gmail.com>
2025-02-18 20:20:41 +0800

    fix: go.mod

---
+++ ts-client/packetforward.v1/types.ts
@@ -0,0 +1,9 @@
+import { Params } from "./types/packetforward/v1/genesis"
+import { InFlightPacket } from "./types/packetforward/v1/genesis"
+
+
+export {     
+    Params,
+    InFlightPacket,
+    
+ }
