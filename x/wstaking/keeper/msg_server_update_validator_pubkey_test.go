3af37f58d4286cea2dd21ed99c72124706f5d04a
JFJun <flynn12jun@gmail.com>
2025-11-18 19:21:28 +0800

    fix: remove test func

--- x/wstaking/keeper/msg_server_update_validator_pubkey_test.go
+++ x/wstaking/keeper/msg_server_update_validator_pubkey_test.go
@@ -1,12 +1,6 @@
 package keeper_test
 
 import (
-	"crypto/sha256"
-	"encoding/base64"
-	"encoding/hex"
-	"fmt"
-	"testing"
-
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	"github.com/st-chain/me-hub/x/wstaking/types"
@@ -63,15 +57,3 @@ func (s *KeeperTestSuite) TestUpdateValidatorPubkey() {
 	}
 
 }
-
-func TestXxx(t *testing.T) {
-	s:="qeWJwMjAL2BDMd+1TnI6agfDa8IGLeVwRXfawHlxZgc="
-	ss,err := base64.StdEncoding.DecodeString(s)
-	if err!=nil{
-		t.Fatal(err)
-	}
-	fmt.Println(len(ss))
-	sss:=sha256.Sum256(ss)
-	fmt.Println(hex.EncodeToString(sss[:20]))
-}
-	
