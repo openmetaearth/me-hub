4a6979c78c28322eaf794cf9d981fbdbf5795584
GAVINNB <42126225+GAVINNB@users.noreply.github.com>
2026-07-01 15:45:41 +0800

    fix(did): enforce comprehensive genesis state validation

--- x/did/types/genesis.go
+++ x/did/types/genesis.go
@@ -1,5 +1,11 @@
 package types
 
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
 // DefaultGenesis returns the default genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
@@ -12,5 +18,89 @@ func DefaultGenesis() *GenesisState {
 
 // Validate performs basic genesis state validation returning an error upon any failure.
 func (gs *GenesisState) Validate() error {
+	// --- validate Infos ---
+	didSet := make(map[string]struct{}, len(gs.Infos))
+	addrSet := make(map[string]struct{}, len(gs.Infos))
+	for i, info := range gs.Infos {
+		if len(info.Did) != DidLength {
+			return fmt.Errorf("infos[%d]: DID length must be %d, got %d", i, DidLength, len(info.Did))
+		}
+		if info.Pubkey == "" {
+			return fmt.Errorf("infos[%d]: pubkey must not be empty", i)
+		}
+		if _, err := sdk.AccAddressFromBech32(info.Address); err != nil {
+			return fmt.Errorf("infos[%d]: invalid address %q: %w", i, info.Address, err)
+		}
+		if _, ok := DidStatus_name[int32(info.Status)]; !ok {
+			return fmt.Errorf("infos[%d]: invalid DID status %d", i, info.Status)
+		}
+		if _, dup := didSet[info.Did]; dup {
+			return fmt.Errorf("infos[%d]: duplicate DID %q", i, info.Did)
+		}
+		didSet[info.Did] = struct{}{}
+		if _, dup := addrSet[info.Address]; dup {
+			return fmt.Errorf("infos[%d]: duplicate address %q", i, info.Address)
+		}
+		addrSet[info.Address] = struct{}{}
+	}
+
+	// --- validate Svcs ---
+	svcSet := make(map[string]struct{}, len(gs.Svcs))
+	for i, svc := range gs.Svcs {
+		if len(svc.Sid) < 2 || len(svc.Sid) > 8 {
+			return fmt.Errorf("svcs[%d]: sid length must be between 2 and 8, got %d", i, len(svc.Sid))
+		}
+		if _, ok := ServiceStatus_name[int32(svc.Status)]; !ok {
+			return fmt.Errorf("svcs[%d]: invalid service status %d", i, svc.Status)
+		}
+		if _, dup := svcSet[svc.Sid]; dup {
+			return fmt.Errorf("svcs[%d]: duplicate service sid %q", i, svc.Sid)
+		}
+		svcSet[svc.Sid] = struct{}{}
+	}
+
+	// --- validate Vcs ---
+	vcSet := make(map[string]struct{}, len(gs.Vcs))
+	for i, vc := range gs.Vcs {
+		if len(vc.Did) != DidLength {
+			return fmt.Errorf("vcs[%d]: DID length must be %d, got %d", i, DidLength, len(vc.Did))
+		}
+		if len(vc.Sid) < 2 || len(vc.Sid) > 8 {
+			return fmt.Errorf("vcs[%d]: sid length must be between 2 and 8, got %d", i, len(vc.Sid))
+		}
+		if len(vc.Hash) == 0 || len(vc.Hash) > 128 {
+			return fmt.Errorf("vcs[%d]: hash length must be between 1 and 128, got %d", i, len(vc.Hash))
+		}
+		if len(vc.Data) > maxCredentialDataLength {
+			return fmt.Errorf("vcs[%d]: data length exceeds %d", i, maxCredentialDataLength)
+		}
+		key := vc.Did + "/" + vc.Sid
+		if _, dup := vcSet[key]; dup {
+			return fmt.Errorf("vcs[%d]: duplicate credential (did=%s, sid=%s)", i, vc.Did, vc.Sid)
+		}
+		vcSet[key] = struct{}{}
+	}
+
+	// --- validate Flogs ---
+	// Each FilterLogger must reference an existing Credential in Vcs to prevent
+	// a panic in InitGenesis when GetCredential returns not-found.
+	flogSet := make(map[string]struct{}, len(gs.Flogs))
+	for i, flog := range gs.Flogs {
+		if len(flog.Did) != DidLength {
+			return fmt.Errorf("flogs[%d]: DID length must be %d, got %d", i, DidLength, len(flog.Did))
+		}
+		if len(flog.Sid) < 2 || len(flog.Sid) > 8 {
+			return fmt.Errorf("flogs[%d]: sid length must be between 2 and 8, got %d", i, len(flog.Sid))
+		}
+		vcKey := flog.Did + "/" + flog.Sid
+		if _, found := vcSet[vcKey]; !found {
+			return fmt.Errorf("flogs[%d]: referenced credential (did=%s, sid=%s) not found in vcs", i, flog.Did, flog.Sid)
+		}
+		if _, dup := flogSet[vcKey]; dup {
+			return fmt.Errorf("flogs[%d]: duplicate filter logger (did=%s, sid=%s)", i, flog.Did, flog.Sid)
+		}
+		flogSet[vcKey] = struct{}{}
+	}
+
 	return nil
 }
--- x/did/types/genesis_test.go
+++ x/did/types/genesis_test.go
@@ -1,6 +1,7 @@
 package types_test
 
 import (
+	"strings"
 	"testing"
 
 	_ "github.com/evmos/ethermint/crypto/ethsecp256k1"
@@ -9,13 +10,20 @@ import (
 	"github.com/openmetaearth/me-hub/x/did/types"
 )
 
-func TestGenesisState_Validate(t *testing.T) {
-	test := types.GenesisState{
+const (
+	testDID     = "1000000000001" // length == DidLength (13)
+	testAddress = "me1kjnt3ypezt3yf58w8upujvejdtt5xsvkq5dpk4"
+	testPubkey  = "{\"@type\":\"/ethermint.crypto.v1.ethsecp256k1.PubKey\",\"key\":\"AjkBriaNQIyoihm/Op5a53ovjdThnbs8G3GhSdErW7Mt\"}"
+)
+
+func validGenesis() types.GenesisState {
+	return types.GenesisState{
 		Infos: []types.DidInfo{
 			{
-				Did:    "1000000000001",
-				Pubkey: "{\"@type\":\"/ethermint.crypto.v1.ethsecp256k1.PubKey\",\"key\":\"AjkBriaNQIyoihm/Op5a53ovjdThnbs8G3GhSdErW7Mt\"}",
-				Status: types.DID_STATUS_ACTIVE,
+				Did:     testDID,
+				Address: testAddress,
+				Pubkey:  testPubkey,
+				Status:  types.DID_STATUS_ACTIVE,
 			},
 		},
 		Svcs: []types.Service{
@@ -23,13 +31,13 @@ func TestGenesisState_Validate(t *testing.T) {
 				Sid:         "kyc",
 				Name:        "kyc",
 				Description: "this is kyc test service.",
-				Issuers:     []string{"1000000000001"},
+				Issuers:     []string{testDID},
 				Status:      types.SERVICE_STATUS_ACTIVE,
 			},
 		},
 		Vcs: []types.Credential{
 			{
-				Did:  "1000000000001",
+				Did:  testDID,
 				Sid:  "kyc",
 				Hash: "0000000000000000001",
 				Uri:  "http://metaearth.com/files/0001.vc",
@@ -38,7 +46,7 @@ func TestGenesisState_Validate(t *testing.T) {
 		},
 		Flogs: []types.FilterLogger{
 			{
-				Did: "000000000001",
+				Did: testDID,
 				Sid: "kyc",
 				Filters: [][]byte{
 					[]byte("A0"),
@@ -46,7 +54,93 @@ func TestGenesisState_Validate(t *testing.T) {
 			},
 		},
 	}
+}
 
-	err := test.Validate()
-	require.NoError(t, err)
+func TestGenesisState_Validate_Valid(t *testing.T) {
+	gs := validGenesis()
+	require.NoError(t, gs.Validate())
+}
+
+func TestGenesisState_Validate_EmptyGenesis(t *testing.T) {
+	gs := types.DefaultGenesis()
+	require.NoError(t, gs.Validate())
+}
+
+func TestGenesisState_Validate_DuplicateDID(t *testing.T) {
+	gs := validGenesis()
+	gs.Infos = append(gs.Infos, gs.Infos[0]) // duplicate DID
+	require.ErrorContains(t, gs.Validate(), "duplicate DID")
+}
+
+func TestGenesisState_Validate_DuplicateAddress(t *testing.T) {
+	gs := validGenesis()
+	// different DID but same address
+	gs.Infos = append(gs.Infos, types.DidInfo{
+		Did:     "9999999999999",
+		Address: testAddress,
+		Pubkey:  testPubkey,
+		Status:  types.DID_STATUS_ACTIVE,
+	})
+	require.ErrorContains(t, gs.Validate(), "duplicate address")
+}
+
+func TestGenesisState_Validate_InvalidDIDLength(t *testing.T) {
+	gs := validGenesis()
+	gs.Infos[0].Did = "short"
+	require.ErrorContains(t, gs.Validate(), "DID length")
+}
+
+func TestGenesisState_Validate_EmptyPubkey(t *testing.T) {
+	gs := validGenesis()
+	gs.Infos[0].Pubkey = ""
+	require.ErrorContains(t, gs.Validate(), "pubkey must not be empty")
+}
+
+func TestGenesisState_Validate_InvalidAddress(t *testing.T) {
+	gs := validGenesis()
+	gs.Infos[0].Address = "not-a-valid-address"
+	require.ErrorContains(t, gs.Validate(), "invalid address")
+}
+
+func TestGenesisState_Validate_DuplicateServiceSid(t *testing.T) {
+	gs := validGenesis()
+	gs.Svcs = append(gs.Svcs, gs.Svcs[0])
+	require.ErrorContains(t, gs.Validate(), "duplicate service sid")
+}
+
+func TestGenesisState_Validate_InvalidServiceSidLength(t *testing.T) {
+	gs := validGenesis()
+	gs.Svcs[0].Sid = "a" // too short
+	require.ErrorContains(t, gs.Validate(), "sid length")
+}
+
+func TestGenesisState_Validate_DuplicateCredential(t *testing.T) {
+	gs := validGenesis()
+	gs.Vcs = append(gs.Vcs, gs.Vcs[0])
+	require.ErrorContains(t, gs.Validate(), "duplicate credential")
+}
+
+func TestGenesisState_Validate_CredentialDataTooLarge(t *testing.T) {
+	gs := validGenesis()
+	gs.Vcs[0].Data = []byte(strings.Repeat("x", 64*1024+1))
+	require.ErrorContains(t, gs.Validate(), "data length exceeds")
+}
+
+func TestGenesisState_Validate_FlogOrphanCredential(t *testing.T) {
+	gs := validGenesis()
+	// flog references a credential that does not exist in Vcs
+	gs.Flogs[0].Did = "9999999999999"
+	require.ErrorContains(t, gs.Validate(), "not found in vcs")
+}
+
+func TestGenesisState_Validate_FlogInvalidDIDLength(t *testing.T) {
+	gs := validGenesis()
+	gs.Flogs[0].Did = "short"
+	require.ErrorContains(t, gs.Validate(), "DID length")
+}
+
+func TestGenesisState_Validate_DuplicateFlog(t *testing.T) {
+	gs := validGenesis()
+	gs.Flogs = append(gs.Flogs, gs.Flogs[0])
+	require.ErrorContains(t, gs.Validate(), "duplicate filter logger")
 }
