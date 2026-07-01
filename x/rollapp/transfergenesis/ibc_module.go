d2bebcf6bf766242118c5ee0c23f9de302548b33
jack <zhong452244751@gmail.com>
2026-07-01 14:56:11 +0800

    fix: update OnRecvPacket return handling and transfer enabling logic (#1436)

--- x/rollapp/transfergenesis/ibc_module.go
+++ x/rollapp/transfergenesis/ibc_module.go
@@ -113,11 +113,11 @@ func (w IBCModule) OnRecvPacket(
 	memo, err := getMemo(transfer.GetMemo())
 	if errorsmod.IsOf(err, gerrc.ErrNotFound) {
 		// The first regular transfer marks the full opening of the bridge, more genesis transfers will not be allowed.
-		err := w.IBCModule.OnRecvPacket(ctx, packet, relayer)
-		if err == nil && !ra.GenesisState.TransfersEnabled {
+		ack := w.IBCModule.OnRecvPacket(ctx, packet, relayer)
+		if (ack == nil || ack.Success()) && !ra.GenesisState.TransfersEnabled {
 			w.rollappKeeper.EnableTransfers(ctx, ra.RollappId)
 		}
-		return err
+		return ack
 	}
 	if err != nil {
 		l.Error("Get memo.", "err", err)
