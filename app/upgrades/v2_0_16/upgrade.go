c35f1b1042c0a694743e084952f575d8d8ca9eae
koelzen <ningxin1718@gmail.com>
2026-08-18 10:52:39 +0800

    fix: upgrade v2.0.16 for beta

--- app/upgrades/v2_0_16/upgrade.go
+++ app/upgrades/v2_0_16/upgrade.go
@@ -51,7 +51,7 @@ func CreateUpgradeHandler(
 			return nil, fmt.Errorf("failed to set Tron max send to external amount: %w", err)
 		}
 
-		skipBscFraudulentBatchEvents(ctx, keepers.BscKeeper)
+		// skipBscFraudulentBatchEvents(ctx, keepers.BscKeeper)
 		logger.Info("upgrade finished successfully.")
 		return mm.RunMigrations(ctx, configurator, fromVM)
 	}
